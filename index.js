6eb1918e4acb8a813426faaa67b975d6bc856704
gabriellesc <gabrielle.singhcadieux@mail.utoronto.ca>
2017-10-01 21:29:33 -0400

    switched npm packages

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 'use strict';
 
-var express = require('express');
-var php = require("php");
-var path = require("path");
+var phpServer = require('node-php-server');
 
 const PORT = process.env.PORT || 3000;
 
-var app = express();
-
-app.use("/", php.cgi("./"));
-
-app.listen(PORT);
-
-app.listen(PORT, function listening() {
-    console.log('Listening on %d', PORT);
+// Create a PHP Server
+phpServer.createServer({
+    port: PORT,
+    hostname: '127.0.0.1',
+    base: '.',
+    keepalive: true,
+    open: false,
+    bin: 'php',
+    router: __dirname + '/RClogo.php'
 });
