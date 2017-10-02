4c6e405ec4954e0494c02be72983562ffcdaa330
gabriellesc <gabrielle.singhcadieux@mail.utoronto.ca>
2017-10-01 21:18:05 -0400

    added npm dependencies

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+'use strict';
+
 var express = require('express');
 var php = require("php");
 var path = require("path");
+
+const PORT = process.env.PORT || 3000;
 
 var app = express();
 
 app.use("/", php.cgi("./"));
 
-app.listen(3000);
+app.listen(PORT);
 
-console.log("Server listening!"); 
+app.listen(PORT, function listening() {
+    console.log('Listening on %d', PORT);
+});
